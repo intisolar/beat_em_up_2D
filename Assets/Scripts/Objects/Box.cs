6733f52d1a7645e5767b80d2b2ba572144f76b12
Guillegau <tecno_guille@hotmail.com>
2025-05-07 21:56:47 -0300

    feat: ataque del jugador

--- Assets/Scripts/Objects/Box.cs
+++ Assets/Scripts/Objects/Box.cs
@@ -1,12 +1,23 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Box : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] private int _life = 100;
+    private float _lastDamageTime;
+    [SerializeField] private float _coolDown = 0.5f;
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Attack") && Time.time >= _lastDamageTime + _coolDown)
         {
-            Destroy(gameObject);
-        }
+            _life--;
+            _lastDamageTime = Time.time;
+            
+            if (_life <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }            
     }
 }
