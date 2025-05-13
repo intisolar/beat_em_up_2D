53520cc4cdd0c72d85009cb0b17a1565cc8ff681
David <141585041+David-Uzno@users.noreply.github.com>
2025-05-12 21:56:24 -0300

    feat: pantalla de game over
    Co-Authored-By: Guillegau <196449358+Guillegau@users.noreply.github.com>

---
+++ Assets/Scripts/UI/Menus/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
