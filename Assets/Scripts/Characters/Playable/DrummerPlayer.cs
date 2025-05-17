8676c9a1fad6602dae97ef2e2b636245c323a18b
intisolar <insolgar@gmail.com>
2025-05-17 00:11:16 -0300

    arreglé las warning

--- Assets/Scripts/Characters/Playable/DrummerPlayer.cs
+++ Assets/Scripts/Characters/Playable/DrummerPlayer.cs
@@ -4,18 +4,7 @@ public class DrummerPlayer : PlayerCharacter
 {
     public override void PerformAttack()
     {
-        throw new System.NotImplementedException();
+      
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
