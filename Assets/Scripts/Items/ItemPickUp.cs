9c030ea46004ee6f8f529f5b317e7ca555e52d94
Guillegau <tecno_guille@hotmail.com>
2025-05-18 21:38:27 -0300

    feat: herencia para los objetos
    Co-Authored-By: David-Uzno <141585041+David-Uzno@users.noreply.github.com>

--- Assets/Scripts/Items/ItemPickUp.cs
+++ Assets/Scripts/Items/ItemPickUp.cs
@@ -2,15 +2,11 @@ using UnityEngine;
 
 public class ItemPickUp : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Objects/Box.cs
+++ Assets/Scripts/Objects/Box.cs
@@ -1,23 +1,7 @@
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Box : MonoBehaviour
+public class Box : Objects  
 {
-    [SerializeField] private int _life = 100;
-    private float _lastDamageTime;
-    [SerializeField] private float _coolDown = 0.5f;
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Attack") && Time.time >= _lastDamageTime + _coolDown)
-        {
-            _life--;
-            _lastDamageTime = Time.time;
-            
-            if (_life <= 0)
-            {
-                Destroy(gameObject);
-            }
-        }            
-    }
+   
 }
