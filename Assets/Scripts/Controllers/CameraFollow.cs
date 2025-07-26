d91e4dff694044e3a1f36ab8b6429a796e3a8218
David <141585041+David-Uzno@users.noreply.github.com>
2025-07-26 11:35:25 -0300

    feat: seguimiento de cámara

--- Assets/Scripts/Controllers/CameraFollow.cs
+++ Assets/Scripts/Controllers/CameraFollow.cs
@@ -1,6 +1,4 @@
-using Unity.VisualScripting;
 using UnityEngine;
-
 
 /// <summary>
 /// Description: Smoothly follows the active player character.
@@ -9,5 +7,38 @@ using UnityEngine;
 /// </summary>
 public class CameraFollow : MonoBehaviour
 {
-    
+    [SerializeField] private bool _canMoveBackward = true;
+
+    [Header("Dependencies")]
+    [SerializeField] private Transform _player;
+
+    private float offsetX;
+    private float fixedY;
+    private float lastX;
+
+    private void Start()
+    {
+        offsetX = transform.position.x - _player.position.x;
+        fixedY = transform.position.y;
+        lastX = _player.position.x;
+    }
+
+    private void LateUpdate()
+    {
+        if (_player != null)
+        {
+            float currentX;
+            if (_canMoveBackward)
+            {
+                currentX = _player.position.x;
+            }
+            else
+            {
+                currentX = Mathf.Max(_player.position.x, lastX);
+            }
+
+            lastX = currentX;
+            transform.position = new Vector3(currentX + offsetX, fixedY, transform.position.z);
+        }
+    }
 }
