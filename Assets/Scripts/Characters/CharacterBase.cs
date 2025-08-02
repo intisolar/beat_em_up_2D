f234a7ade6e3f429468a29c7a294276c0d573743
David <141585041+David-Uzno@users.noreply.github.com>
2025-08-02 19:34:37 -0300

    Update CharacterBase.cs

--- Assets/Scripts/Characters/CharacterBase.cs
+++ Assets/Scripts/Characters/CharacterBase.cs
@@ -10,35 +10,31 @@ using UnityEngine;
 public abstract class CharacterBase : MonoBehaviour, IAggressive, IDamageable
 {
     #region Variables
-    [Header("Stats")]
+    [Header("Health Stats")]
     [SerializeField] private int _maxHealth = 10;
     public int MaxHealth { get; private set; }
-
     [SerializeField] private int _currentHealth;
     public int CurrentHealth { get; private set; }
 
+    [Header("Combat Stats")]
     [SerializeField] private float _attackPower = 10;
     public float AttackPower { get; private set; }
-
     [SerializeField] private float _defense = 5;
     public float Defense { get; private set; }
-
-    [SerializeField] private float _moveSpeed = 1f;
-    public float MoveSpeed { get; private set; }
-
-    [SerializeField] private float _attackSpeed = 1f;
-    public float AttackSpeed { get; private set; }
-
     [SerializeField] private float _knockbackPower = 5f;
     public float KnockbackPower { get; private set; }
+
+    [Header("Movement Stats")]
+    [SerializeField] private float _moveSpeed = 1f;
+    public float MoveSpeed { get; private set; }
+    [SerializeField] private float _attackSpeed = 1f;
+    public float AttackSpeed { get; private set; }
 
     [Header("Attack Data")]
     [SerializeField] private LayerMask _targetLayers;
     public LayerMask TargetLayers { get; private set; }
-
     [SerializeField] private Transform _weapon;
     public Transform Weapon { get; private set; }
-
     [SerializeField] private float _range = 0.5f;
     public float Range { get; private set; }
     #endregion
